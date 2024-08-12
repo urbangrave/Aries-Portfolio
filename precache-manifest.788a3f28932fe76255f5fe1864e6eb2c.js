0d7975c2292aeb3049fa933a5b724b4cee3578ab
unknown <ariesdave.bantigue.sdtpnoli@gmail.com>
2024-08-12 11:25:48 +0800

    Updates

---
+++ precache-manifest.788a3f28932fe76255f5fe1864e6eb2c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ea18f96a83f7b0108c04166a77c26d",
+    "url": "/aries-portfolio/index.html"
+  },
+  {
+    "revision": "58b91d68291927e91569",
+    "url": "/aries-portfolio/static/css/2.5175cca5.chunk.css"
+  },
+  {
+    "revision": "6e95aa82dc1c6187a1f7",
+    "url": "/aries-portfolio/static/css/main.bda03402.chunk.css"
+  },
+  {
+    "revision": "58b91d68291927e91569",
+    "url": "/aries-portfolio/static/js/2.c8e51138.chunk.js"
+  },
+  {
+    "revision": "6e95aa82dc1c6187a1f7",
+    "url": "/aries-portfolio/static/js/main.b0076533.chunk.js"
+  },
+  {
+    "revision": "985de23b75813c6f3f96",
+    "url": "/aries-portfolio/static/js/runtime~main.cf953ddd.js"
+  },
+  {
+    "revision": "ef062dee587aa83c521c63da5bcac375",
+    "url": "/aries-portfolio/static/media/Autecy.ef062dee.PNG"
+  },
+  {
+    "revision": "59867bee3e6ac4fddb1f1080b0142ca8",
+    "url": "/aries-portfolio/static/media/BMS.59867bee.png"
+  },
+  {
+    "revision": "c0a91dd0101a586d54fdfbef75be2aab",
+    "url": "/aries-portfolio/static/media/Bookkeeping.c0a91dd0.png"
+  },
+  {
+    "revision": "d279ff632b01979c200c2da0da8bdd4e",
+    "url": "/aries-portfolio/static/media/Dts.d279ff63.png"
+  },
+  {
+    "revision": "ff61d2860bde30a53cf695af9664ddd7",
+    "url": "/aries-portfolio/static/media/GH.ff61d286.svg"
+  },
+  {
+    "revision": "f780c213eccff630a5a58930f962f5f4",
+    "url": "/aries-portfolio/static/media/Psds.f780c213.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/aries-portfolio/static/media/defaultNavLogo.06e73328.svg"
+  },
+  {
+    "revision": "6c7d72e66a546a8ca8c54c5956becb50",
+    "url": "/aries-portfolio/static/media/hero-dark.6c7d72e6.jpg"
+  },
+  {
+    "revision": "4cb9e4a324c6803f7ae1d160a712bbd1",
+    "url": "/aries-portfolio/static/media/hero-light.4cb9e4a3.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/aries-portfolio/static/media/logo.06e73328.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/aries-portfolio/precache-manifest.788a3f28932fe76255f5fe1864e6eb2c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aries-portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
