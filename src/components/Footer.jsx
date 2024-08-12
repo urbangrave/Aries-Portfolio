b2be0483df9a0ed840242779a9b8b03db203122d
unknown <ariesdave.bantigue.sdtpnoli@gmail.com>
2024-08-12 11:24:10 +0800

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from "styled-components";
+// Components
+import SocialLinks from "./SocialLinks";
+
+const StyledFooter = styled.footer`
+  min-height: var(--min-footer-height);
+  background: var(--primary);
+
+  a {
+    color: #45413c;
+
+    &:hover {
+      color: #fbfdff;
+    }
+  }
+`;
+
+export default function Footer() {
+  return (
+    <StyledFooter className="d-flex align-items-center justify-content-center p-2">
+      <SocialLinks />
+    </StyledFooter>
+  );
+}
